23984d4a700e3e2fe096e218f5825d941c34b0c8
ARezantsev <99681161+ARezantsev@users.noreply.github.com>
2022-02-16 21:34:55 +0200

    J1

--- J1.js
+++ J1.js
@@ -2,9 +2,9 @@
 const b = a * a * 3.14159;
 console.log(b);*/
 
-/*const year = prompt ('Введите год');
+const year = prompt ('Введите год');
 const yearshp = 2022 - year;
-console.log(yearshp);*/
+console.log(yearshp);
 
 const year = prompt('Введите Ваш возраст');
 const pension = 65 - year;
