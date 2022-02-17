cc6e5017f1b2057051e86c8b9453e9ae5a18b177
ARezantsev <99681161+ARezantsev@users.noreply.github.com>
2022-02-17 19:21:22 +0200

    Update J1.js

--- J1.js
+++ J1.js
@@ -1,11 +1,2 @@
-// const a = prompt('Введите радиус круга');
-// const b = a * a * 3.14159;
-// console.log(b);
 
-// const year = prompt ('Введите год');
-// const yearshp = 2022 - year;
-// console.log(yearshp);
 
-const year = prompt('Введите Ваш возраст');
-const pension = 65 - year;
-console.log(pension, pension + 2022); 
